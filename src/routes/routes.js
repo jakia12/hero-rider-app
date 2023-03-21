5a5d389348ab250e3e205d0963c644c67e9e4b63
jakia12 <owasi110@gmail.com>
2023-03-21 13:59:45 +0600

    images added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,11 +11,96 @@ import SignUp from "../pages/signUp/RiderSignUp";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Home />}>
-      <Route path="/login" element={<Login />} />
-      <Route path="/riderSignUp" element={<RiderSignUp />} />
-      <Route path="/learnerSignUp" element={<LearnerSignUp />} />
-      <Route path="/login" element={<Login />} />
-    </Route>
+    <>
+      <Route path="/" element={<Home />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/riderSignUp" element={<RiderSignUp />} />
+        <Route path="/learnerSignUp" element={<LearnerSignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+      {/* admin dashboard route */}
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <DashboardLayout />
+          </PrivateRoute>
+        }
+        errorElement={<DislayError />}
+      >
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Overview />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard/myOrders"
+          element={
+            <BuyerRoute>
+              <MyOrder />
+            </BuyerRoute>
+          }
+        />
+        <Route
+          path="/dashboard/payment/:id"
+          element={
+            <PrivateRoute>
+              <Payment />
+            </PrivateRoute>
+          }
+          loader={paymentLoader}
+        ></Route>
+
+        <Route
+          path="/dashboard/addProducts"
+          element={
+            <SellerRoute>
+              <AddProduct />
+            </SellerRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard/myProducts"
+          element={
+            <SellerRoute>
+              <MyProduct />
+            </SellerRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard/allSellers"
+          element={
+            <AdminRoute>
+              <AllSellers />
+            </AdminRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard/allBuyers"
+          element={
+            <AdminRoute>
+              <AllBuyers />
+            </AdminRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard/reportedProducts"
+          element={
+            <AdminRoute>
+              <ReportedProducts />
+            </AdminRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </>
   )
 );

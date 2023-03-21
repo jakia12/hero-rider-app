b43e14f62d3a355d157309e7e4a4b4ece0785c1c
jakia12 <owasi110@gmail.com>
2023-03-21 10:49:13 +0600

    learner sign up page added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,14 +4,17 @@ import {
   Route,
 } from "react-router-dom";
 import Login from "../pages/login/Login";
+import LearnerSignUp from "../pages/signUp/LearnerSignUp";
+import RiderSignUp from "../pages/signUp/RiderSignUp";
 
-import SignUp from "../pages/signUp/SignUp";
+import SignUp from "../pages/signUp/RiderSignUp";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Home />}>
       <Route path="/login" element={<Login />} />
-      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/riderSignUp" element={<RiderSignUp />} />
+      <Route path="/learnerSignUp" element={<LearnerSignUp />} />
       <Route path="/login" element={<Login />} />
     </Route>
   )
